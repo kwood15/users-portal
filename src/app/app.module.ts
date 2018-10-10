f8507ad021b6318d8f00bd4467d246bd4f0f87f2
Kay Wood <11405108+kwood15@users.noreply.github.com>
2018-10-10 16:30:54 +0100

    Add animations to the Users Component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,7 +1,8 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-import { AppRoutingModule } from './app-routing.module';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { HttpClientModule } from '@angular/common/http';
+import { AppRoutingModule } from './app-routing.module';
 
 import { AppComponent } from './app.component';
 import { SidebarComponent } from './sidebar/sidebar.component';
@@ -21,7 +22,8 @@ import { UsersComponent } from './users/users.component';
   imports: [
     AppRoutingModule,
     BrowserModule,
-    HttpClientModule
+    HttpClientModule,
+    BrowserAnimationsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
--- src/app/users/users.component.ts
+++ src/app/users/users.component.ts
@@ -1,11 +1,24 @@
 import { Component, OnInit } from '@angular/core';
+import { trigger, style, transition, animate, keyframes, query, stagger } from '@angular/animations';
 import { Observable } from 'rxjs';
 
 import { DataService } from '../data.service';
 @Component({
   selector: 'app-users',
   templateUrl: './users.component.html',
-  styleUrls: ['./users.component.scss']
+  styleUrls: ['./users.component.scss'],
+  animations: [
+    trigger('listStagger', [
+      transition('* <=> *', [
+        query(':enter',
+          [
+            style({ opacity: 0, transform: 'translateY(-15px)' }),
+            stagger('50ms', animate('550ms ease-out', style({ opacity: 1, transform: 'translateY(0px)' })))
+          ], { optional: true }),
+        query(':leave', animate('50ms', style({ opacity: 0 })), { optional: true })
+      ])
+    ])
+  ]
 })
 
 export class UsersComponent implements OnInit {
